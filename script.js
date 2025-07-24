cb857e123254c20c50fdc9702b5f8ed333a29fbc
Ankita Das <daszankita23@gmail.com>
2025-07-24 17:30:36 +0530

    js added

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.querySelector("#btn");
+const sts = document.querySelector("h5");
+let check = 0;
+button.addEventListener("click", function () {
+  if (check == 0) {
+    sts.innerHTML = "Friends";
+    sts.style.color = "green";
+    button.innerHTML = "Remove";
+    check = 1;
+  } else {
+    sts.innerHTML = "Stranger";
+    sts.style.color = "Red";
+    button.innerHTML = "Add Friend";
+    check = 0;
+  }
+});
