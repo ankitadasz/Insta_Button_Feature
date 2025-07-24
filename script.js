30104366cf2fe5365dbd2f3b82f7caca86fd9538
Ankita Das <daszankita23@gmail.com>
2025-07-24 17:31:07 +0530

    js added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sts = document.querySelector("h5");
 let check = 0;
 button.addEventListener("click", function () {
   if (check == 0) {
-    sts.innerHTML = "Friends";
+    sts.innerHTML = "Friend";
     sts.style.color = "green";
     button.innerHTML = "Remove";
     check = 1;
