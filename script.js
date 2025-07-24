0de4f80797f9cd62c10b9a1f2a5eacab693d392c
Ankita Das <daszankita23@gmail.com>
2025-07-24 18:15:23 +0530

    love icon working properly

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ button.addEventListener("click", function () {
     check = 0;
   }
 });
+
+const img=document.querySelector("#img-container");
+const love=document.querySelector("i");
+img.addEventListener("dblclick",function(){
+    love.style.transform="translate(-50%,-50%) scale(1)";
+    love.style.opacity="1"
+    setTimeout(function(){
+    love.style.transform="translate(-50%,-50%) scale(0)";
+    love.style.opacity="0";
+},1000)
+    
+})
